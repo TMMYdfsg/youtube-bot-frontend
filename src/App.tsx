248a32b246aa619b12f56f487f2b2ba0a6ab719d
TMMYdfsg <haruka_tomoya_714_810_1226@yahoo.co.jp>
2025-06-24 23:00:39 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import BotStatus from "./components/BotStatus";
 import ChatLog from "./components/ChatLog";
 import LiveStreamPlayer from "./components/LiveStreamPlayer";
+import ManualChatForm from "./components/ManualChatForm";
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       <h1>YouTube Bot 管理画面</h1>
       <BotStatus />
       <ChatLog />
-      <LiveStreamPlayer /> {/* ← ライブ動画を表示 */}
+      <LiveStreamPlayer />
+      <ChatInput />
+      <ManualChatForm /> {/* ← ここを追加 */}
     </div>
   );
 }
-
-export default App;
