cb73b5e37157c129773d89c9a167fe9a501ca499
TMMYdfsg <haruka_tomoya_714_810_1226@yahoo.co.jp>
2025-06-25 00:04:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import BotStatus from "./components/BotStatus";
 import ChatLog from "./components/ChatLog";
 import LiveStreamPlayer from "./components/LiveStreamPlayer";
 import ManualChatForm from "./components/ManualChatForm";
+import GameSelector from "./components/GameSelector";
+import KeywordHighlighter from "./components/KeywordHighlighter";
+import ChatInput from "./components/ChatInput";
 
 function App() {
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
+  const [selectedGame, setSelectedGame] = useState<string>("");
+
   return (
     <div className="App">
       <h1>YouTube Bot 管理画面</h1>
       <BotStatus />
+      <LiveVideo />
+      <GameSelector onKeywordsSelected={(keywords, game) => {
+        setSelectedKeywords(keywords);
+        setSelectedGame(game);
+      }} />
+      <KeywordHighlighter keywords={selectedKeywords} game={selectedGame} />
       <ChatLog />
-      <LiveStreamPlayer />
       <ChatInput />
-      <ManualChatForm /> {/* ← ここを追加 */}
     </div>
   );
 }
