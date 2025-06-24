43ab7cb6fbd8a2c74c7c2d17c2b38d634f276b70
TMMYdfsg <haruka_tomoya_714_810_1226@yahoo.co.jp>
2025-06-24 16:22:40 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@
 import React from 'react';
 import BotStatus from './components/BotStatus';
 import ChatLog from './components/ChatLog';
-import ManualChatForm from './components/ManualChatForm'; // フォームをインポート
+import ManualChatForm from './components/ManualChatForm';
+import LiveStreamPlayer from './components/LiveStreamPlayer'; // ★★★ 1. プレイヤーをインポート
 
 function App() {
   return (
     <div className="App">
       <h1>YouTube Live Bot 管理画面</h1>
       <hr />
+      <LiveStreamPlayer /> {/* ★★★ 2. 好きな場所にプレイヤーを追加 */}
+      <hr />
       <BotStatus />
       <hr />
-      <ManualChatForm /> {/* フォームコンポーネントを追加 */}
+      <ManualChatForm />
       <hr />
       <ChatLog />
     </div>
